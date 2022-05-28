c5bd442402fe15a716e82f2e33d06f9e9c2b2bd1
wisdom <wisdommaliki@gmail.com>
2022-05-27 18:37:50 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/navigation/navbar";
+import Header from "./components/header/header";
+import ParticlesBG from "./components/particles/particles_bg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+
+      <Header />
+
+    </>
   );
 }
 
---
+++ src/components/particles/particles_bg.js
@@ -0,0 +1,8 @@
+import Particles from "react-tsparticles"
+import ParticleConfig from "./config"
+const ParticlesBG = () => {
+
+  return (<Particles params={ParticleConfig}>
+  </Particles>)
+}
+export default ParticlesBG
